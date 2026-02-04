ba1465ade3fb3c1ff768eea6d3474e6c7fc8da3b
nvhao100604 <164126733+nvhao100604@users.noreply.github.com>
2026-02-04 20:31:17 +0700

    fix env reading

--- src/config/constants/api/index.ts
+++ src/config/constants/api/index.ts
@@ -1,11 +1,10 @@
-const envCheck = (key: string, fallback_value: string): string => {
-    const value = process.env[key]
+const envCheck = (value: string | undefined, fallback_value: string): string => {
     if (!value) {
         if (fallback_value) {
-            console.warn(`⚠️ [ENV WARNING] Missing "${key}". Using: "${fallback_value}"`)
+            console.warn(`⚠️ [ENV WARNING] Missing "${value}". Using: "${fallback_value}"`)
             return fallback_value
         } else {
-            const message = `❌ [ENV ERROR] Missing required environment variable: "${key}"`;
+            const message = `❌ [ENV ERROR] Missing required environment variable: "${value}"`;
             console.error(message);
 
             return "";
@@ -15,25 +14,25 @@ const envCheck = (key: string, fallback_value: string): string => {
 }
 
 export const BASE_SERVER_URL = envCheck(
-    "NEXT_PUBLIC_API_BASE_URL",
-    "http://localhost:8080/api/v1/"
+    process.env.NEXT_PUBLIC_API_BASE_URL,
+    "localhost:8080/api/v1/"
 )
 
 // Timeout: Cần chuyển từ string sang number
 export const DEFAULT_TIMEOUT = Number(envCheck(
-    "NEXT_PUBLIC_API_TIMEOUT",
+    process.env.NEXT_PUBLIC_API_TIMEOUT,
     "8000"
 ))
 
 // Delay giả lập
 export const RESPONSE_DELAY = Number(envCheck(
-    "NEXT_PUBLIC_RESPONSE_DELAY",
+    process.env.NEXT_PUBLIC_RESPONSE_DELAY,
     "2000"
 ))
 
 // Thời gian refresh token/data
 export const REFRESH_INTERVAL = Number(envCheck(
-    "NEXT_PUBLIC_REFRESH_INTERVAL",
+    process.env.NEXT_PUBLIC_REFRESH_INTERVAL,
     "60000"
 ))
 
